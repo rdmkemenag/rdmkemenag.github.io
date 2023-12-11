cc9882c227a499a9211aa2eb2bee5a281cb32651
rdmkemenag <153454269+rdmkemenag@users.noreply.github.com>
2023-12-11 13:15:02 +0700

    Add files via upload

---
+++ js/kang-is-feed.js
@@ -0,0 +1,21 @@
+var multiFeed = {
+    feedsUri: [
+        {
+            name: "AMPHTML",
+            url: "https://www.rdmkemenag.com/",
+            tag: ""
+        }
+    ],
+    numPost: 4,
+    showThumbnail: true,
+    showSummary: false,
+    summaryLength: 80,
+    titleLength: "auto",
+    thumbSize: 72,
+    newTabLink: true,
+    containerId: "feed-list-container",
+    readMore: {
+        text: "Selengkapnya",
+        endParam: "?max-results=8"
+    }
+};
